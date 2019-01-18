3dceb1ed8427520f3b1cdb76324bdfc2fff51d8b
Tamas Papik <trapacska@gmail.com>
2019-01-18 13:29:22 +0100

    initial step version (#1)
    * initial step version
    
    * PR changes
    
    * PR changes

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+	shellquote "github.com/kballard/go-shellquote"
+)
+
+type config struct {
+	AdditionalParams string `env:"additional_params"`
+	ProjectLocation  string `env:"project_location,dir"`
+}
+
+func failf(msg string, args ...interface{}) {
+	log.Errorf(msg, args...)
+	os.Exit(1)
+}
+
+func main() {
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
+	}
+	stepconf.Print(cfg)
+
+	additionalParams, err := shellquote.Split(cfg.AdditionalParams)
+	if err != nil {
+		failf("Failed to parse additional parameters, error: %s", err)
+	}
+
+	fmt.Println()
+	log.Infof("Running analyze")
+
+	analyzeCmd := command.New("flutter", append([]string{"analyze"}, additionalParams...)...).
+		SetStdout(os.Stdout).
+		SetStderr(os.Stderr).
+		SetDir(cfg.ProjectLocation)
+
+	fmt.Println()
+	log.Donef("$ %s", analyzeCmd.PrintableCommandArgs())
+	fmt.Println()
+
+	if err := analyzeCmd.Run(); err != nil {
+		failf("Running command failed, error: %s", err)
+	}
+}
